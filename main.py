7c0de9b35d67e2c72081e85646695c542d34610e
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-05-03 14:23:26 +0000

    Exercício Resolvido

--- main.py
+++ main.py
@@ -2,11 +2,26 @@
 
 def count_vowels(string:str) -> int:
     """
-    Takes in a string and returns the number of vowels in the string.
+        Takes in a string and returns the number of vowels in the string.
 
-    Args:
-        string (str): The string to count vowels in.
+        Args:
+            string (str): The string to count vowels in.
 
-    Returns:
-        int: The number of vowels in the string.
-    """
+        Returns:
+            int: The number of vowels in the string.
+        """
+    count = 0
+    vowels = {"a","e","i","o","u"}
+    status = True
+
+    while status:
+        for v in vowels:
+            count += string.count(v)
+        status = False
+
+    return print(count)
+
+
+if __name__ == "__main__" :
+    strTest = input("Aonde você quer contar? \n")
+    count_vowels(strTest)
