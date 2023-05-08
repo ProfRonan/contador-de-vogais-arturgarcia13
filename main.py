9496151adfd1b62391f1d17c0ff9bce79c50d7f3
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-05-08 10:35:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This program counts the number of vowels in a string."""
 
 def count_vowels(string:str) -> int:
+    stringF = string.casefold()
     """
         Takes in a string and returns the number of vowels in the string.
 
@@ -10,16 +11,18 @@ def count_vowels(string:str) -> int:
         Returns:
             int: The number of vowels in the string.
         """
+    print(f'Irei contar aqui {stringF}\n')
     count = 0
     vowels = {"a","e","i","o","u"}
     status = True
 
     while status:
         for v in vowels:
-            count += string.count(v)
+            count += stringF.count(v)
+            print(f'Ja encontrei {count} vogais')
         status = False
 
-    return count
+    return print(count)
 
 
 if __name__ == "__main__" :
